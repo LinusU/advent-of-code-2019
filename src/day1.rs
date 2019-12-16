3bd247c08d8496905c8bb25efcd84968f4d858a3
Linus Unnebäck <linus@folkdatorn.se>
2019-12-16 19:18:43 +0000

    🎉 Add solution to Day 1 - Part 2

--- src/day1.rs
+++ src/day1.rs
@@ -12,3 +12,23 @@ pub fn part1(input: &str) -> Result<u64, ParseIntError> {
         .map(|mass| (mass / 3) - 2)
         .sum())
 }
+
+fn fuel_cost(mass: u64) -> u64 {
+    if mass < 9 {
+        0
+    } else {
+        let fuel = (mass / 3) - 2;
+        fuel + fuel_cost(fuel)
+    }
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &str) -> Result<u64, ParseIntError> {
+    Ok(input
+        .split_whitespace()
+        .map(|mass| mass.parse::<u64>())
+        .collect::<Result<Vec<u64>, ParseIntError>>()?
+        .iter()
+        .map(|mass| fuel_cost(*mass))
+        .sum())
+}
