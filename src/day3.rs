da46884e179a99bdef32613eb6f125cfcaa047e5
Linus Unnebäck <linus@folkdatorn.se>
2019-12-17 12:04:39 +0000

    🎉 Add solution to Day 3 - Part 2

--- src/day3.rs
+++ src/day3.rs
@@ -69,18 +69,40 @@ impl FromStr for ProgramInput {
 }
 
 #[derive(Clone, Copy)]
+enum PathSegmentDirection {
+    Ascending,
+    Descending,
+}
+
+#[derive(Clone, Copy)]
 enum PathSegment {
-    Horizontal { y: isize, x_lo: isize, x_hi: isize },
-    Vertical { x: isize, y_lo: isize, y_hi: isize },
+    Horizontal { y: isize, x_lo: isize, x_hi: isize, dir: PathSegmentDirection },
+    Vertical { x: isize, y_lo: isize, y_hi: isize, dir: PathSegmentDirection },
 }
 
 impl PathSegment {
+    pub fn len(self) -> usize {
+        match self {
+            PathSegment::Horizontal { x_lo, x_hi, .. } => (x_hi - x_lo) as usize,
+            PathSegment::Vertical { y_lo, y_hi, .. } => (y_hi - y_lo) as usize,
+        }
+    }
+
+    pub fn offset_of(self, point: (isize, isize)) -> usize {
+        match self {
+            PathSegment::Horizontal { dir: PathSegmentDirection::Ascending, x_lo, .. } => (point.0 - x_lo) as usize,
+            PathSegment::Horizontal { dir: PathSegmentDirection::Descending, x_hi, .. } => (x_hi - point.0) as usize,
+            PathSegment::Vertical { dir: PathSegmentDirection::Ascending, y_lo, .. } => (point.1 - y_lo) as usize,
+            PathSegment::Vertical { dir: PathSegmentDirection::Descending, y_hi, .. } => (y_hi - point.1) as usize,
+        }
+    }
+
     pub fn intersection(self, other: PathSegment) -> Option<(isize, isize)> {
         match (self, other) {
-            (PathSegment::Horizontal { y, x_lo, x_hi }, PathSegment::Vertical { y_lo, y_hi, x }) => {
+            (PathSegment::Horizontal { y, x_lo, x_hi, .. }, PathSegment::Vertical { y_lo, y_hi, x, .. }) => {
                 if y_lo <= y && y <= y_hi && x_lo <= x && x <= x_hi { Some((x, y)) } else { None }
             }
-            (PathSegment::Vertical { x, y_lo, y_hi }, PathSegment::Horizontal { x_lo, x_hi, y }) => {
+            (PathSegment::Vertical { x, y_lo, y_hi, .. }, PathSegment::Horizontal { x_lo, x_hi, y, .. }) => {
                 if x_lo <= x && x <= x_hi && y_lo <= y && y <= y_hi { Some((x, y)) } else { None }
             }
             _ => None
@@ -105,19 +127,19 @@ impl From<InstructionSet> for Path {
         for item in input {
             match item {
                 Instruction::Up(v) => {
-                    result.push(PathSegment::Vertical { x, y_lo: y, y_hi: y + v });
+                    result.push(PathSegment::Vertical { x, y_lo: y, y_hi: y + v, dir: PathSegmentDirection::Ascending });
                     y += v;
                 }
                 Instruction::Down(v) => {
-                    result.push(PathSegment::Vertical { x, y_lo: y - v, y_hi: y });
+                    result.push(PathSegment::Vertical { x, y_lo: y - v, y_hi: y, dir: PathSegmentDirection::Descending });
                     y -= v;
                 }
                 Instruction::Left(v) => {
-                    result.push(PathSegment::Horizontal { y, x_lo: x - v, x_hi: x });
+                    result.push(PathSegment::Horizontal { y, x_lo: x - v, x_hi: x, dir: PathSegmentDirection::Descending });
                     x -= v;
                 }
                 Instruction::Right(v) => {
-                    result.push(PathSegment::Horizontal { y, x_lo: x, x_hi: x + v });
+                    result.push(PathSegment::Horizontal { y, x_lo: x, x_hi: x + v, dir: PathSegmentDirection::Ascending });
                     x += v;
                 }
             }
@@ -149,6 +171,39 @@ pub fn part1(input: &str) -> Result<u64, ParseIntError> {
     Ok(min_distance as u64)
 }
 
+#[aoc(day3, part2)]
+pub fn part2(input: &str) -> Result<u64, ParseIntError> {
+    let input: ProgramInput = input.parse()?;
+
+    let first_path: Path = input.first.into();
+    let second_path: Path = input.second.into();
+
+    let mut min_distance = usize::max_value();
+
+    let mut first_len = 0;
+    for segment in first_path.iter() {
+        let mut second_len = 0;
+
+        for other in second_path.iter() {
+            if let Some(intersection) = segment.intersection(*other) {
+                if intersection == (0, 0) { continue; }
+
+                let first_offset = segment.offset_of(intersection);
+                let second_offset = other.offset_of(intersection);
+
+                let distance = first_len + first_offset + second_len + second_offset;
+                min_distance = std::cmp::min(min_distance, distance);
+            }
+
+            second_len += other.len();
+        }
+
+        first_len += segment.len();
+    }
+
+    Ok(min_distance as u64)
+}
+
 #[cfg(test)]
 mod test {
     #[test]
@@ -159,5 +214,15 @@ mod test {
     #[test]
     fn part_1_should_give_159() {
         assert_eq!(super::part1("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83"), Ok(159));
+    }
+
+    #[test]
+    fn part_2_should_give_610() {
+        assert_eq!(super::part2("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83"), Ok(610));
+    }
+
+    #[test]
+    fn part_2_should_give_410() {
+        assert_eq!(super::part2("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7"), Ok(410));
     }
 }
