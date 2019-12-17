3f7e2a4c2234ec609f42b218af2c3b4b840930c5
Linus Unnebäck <linus@folkdatorn.se>
2019-12-17 17:20:25 +0000

    🎉 Add solution to Day 4 - Part 2

--- src/day4.rs
+++ src/day4.rs
@@ -38,11 +38,32 @@ fn password_is_valid_v1(password: u64) -> bool {
     has_two_adjecent && never_decreases
 }
 
+fn password_is_valid_v2(password: u64) -> bool {
+    let digit0 = (password / 100_000) % 10;
+    let digit1 = (password / 10_000) % 10;
+    let digit2 = (password / 1_000) % 10;
+    let digit3 = (password / 100) % 10;
+    let digit4 = (password / 10) % 10;
+    let digit5 = password % 10;
+
+    let has_two_adjecent = (digit0 == digit1 && digit1 != digit2) || (digit0 != digit1 && digit1 == digit2 && digit2 != digit3) || (digit1 != digit2 && digit2 == digit3 && digit3 != digit4) || (digit2 != digit3 && digit3 == digit4 && digit4 != digit5) || (digit3 != digit4 && digit4 == digit5);
+    let never_decreases = (digit0 <= digit1) && (digit1 <= digit2) && (digit2 <= digit3) && (digit3 <= digit4) && (digit4 <= digit5);
+
+    has_two_adjecent && never_decreases
+}
+
 #[aoc(day4, part1)]
 pub fn part1(input: &str) -> Result<u64, ParseIntError> {
     let bounds: Bounds = input.parse()?;
 
     Ok(bounds.count_valid_passwords(&password_is_valid_v1) as u64)
+}
+
+#[aoc(day4, part2)]
+pub fn part2(input: &str) -> Result<u64, ParseIntError> {
+    let bounds: Bounds = input.parse()?;
+
+    Ok(bounds.count_valid_passwords(&password_is_valid_v2) as u64)
 }
 
 #[cfg(test)]
@@ -60,5 +81,20 @@ mod test {
     #[test]
     fn part_1_should_give_3() {
         assert_eq!(super::part1("111111-111113"), Ok(3));
+    }
+
+    #[test]
+    fn part_2_should_give_0() {
+        assert_eq!(super::part2("123444-123444"), Ok(0));
+    }
+
+    #[test]
+    fn part_2_should_give_1() {
+        assert_eq!(super::part2("112233-112233"), Ok(1));
+    }
+
+    #[test]
+    fn part_2_should_give_22() {
+        assert_eq!(super::part2("111122-111322"), Ok(22));
     }
 }
